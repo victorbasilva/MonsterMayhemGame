efa989e546e47365073dbb9a2505d6e670023c57
victorbasilva <102388646+victorbasilva@users.noreply.github.com>
2024-05-08 22:59:05 +0100

    Initialization - Main Class

---
+++ src/main/java/com/mycompany/monstermayhemgame/MonsterMayhemGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.monstermayhemgame;
+
+/**
+ * Concurrent Systems - CA2
+ * @author Victor - 2021259
+ */
+public class MonsterMayhemGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
